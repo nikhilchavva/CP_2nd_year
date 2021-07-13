4154f040fb8548bb141d8716fcc4b7726332bf6b
nikhilchavva <nikhilchavva@msitprogram.net>
2021-07-13 11:51:02 +0530

    isfactor coded

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,16 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	if(f==0 and n==0):
+		return True
+	elif(f==n):
+		return True
+	elif(f==0):
+		return False
+	elif(n==0):
+		return True
+	elif (n%f==0):
+		return True
+	else:
+		return False
+print(fun_isfactor(2, 4))
