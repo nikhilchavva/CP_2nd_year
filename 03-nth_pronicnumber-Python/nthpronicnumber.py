c10d931113b7cf6d7450e92902b44e1309bef1a7
nikhilchavva <nikhilchavva@msitprogram.net>
2021-08-04 21:23:33 +0530

    coded

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -3,6 +3,16 @@
 # Number. Pronic number is a number which is the product of two consecutive integers, that is, a 
 # number n is a product of x and (x+1).
 
+def pronicnumber(x):
+	return x*(x+1)
+
 def nthpronicnumber(n):
 	# Your code goes here
-	pass
+	
+	result=0
+	guess=0
+	while(1):
+		result=pronicnumber(guess)
+		if(guess==n):
+			return result
+		guess+=1
